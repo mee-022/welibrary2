37908b2cb062ab94d4e87418911312fac5a36211
mee-022 <jycfp268@yahoo.co.jp>
2020-09-02 15:25:40 +0900

    add comment&search

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -7,4 +7,12 @@ class Tweet < ApplicationRecord
 
   belongs_to :user
   has_many :comments
+
+  def self.search(search)
+    if search != ""
+      Tweet.where(['title LIKE(?) OR author LIKE(?) OR text LIKE(?)', "%#{search}%", "%#{search}%", "%#{search}%"])
+    else
+      Tweet.all
+    end
+  end
 end
